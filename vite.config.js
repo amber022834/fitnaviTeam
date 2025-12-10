b159a55e744efb4e5cdc82f83d00ee745e770fdd
amber022834 <sodagreen0228@gmail.com>
2025-12-10 21:58:01 +0800

    更新base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  // base: './',
+  base: '/fitnaviTeam/',
   plugins: [react()],
 
 })
